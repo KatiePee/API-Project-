888cfa85e0a5e42a7dada95737ab42f4f536d5ae
KatiePee <piele.katie@gmail.com>
2023-04-19 11:10:17 -0700

    added scopes to image tables to exclude, id, reated and updated at

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -34,6 +34,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'SpotImage',
+    defaultScope: {
+      attributes: {
+        exclude: ['spotId', 'createdAt', 'updatedAt']
+      }
+    }
   });
   return SpotImage;
 };
