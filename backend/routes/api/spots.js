4dc1f955749ea07dcf6eb9d5c40d0ca189dfd481
KatiePee <piele.katie@gmail.com>
2023-04-19 20:55:06 -0700

    Get all spots completed and tested

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,10 +6,42 @@
 const express = require('express');
 const router = express.Router();
 const { Spot, Review, SpotImage } = require('../../db/models');
+const Sequelize =require('../../db/models')
 
 router.get('/', async(_req, res, next) => {
-    const spots = await Spot.findAll();
-    res.json(spots)
+    const spotsPromise = await Spot.findAll({
+        include: [
+           {
+            model: Review,
+            attributes: ['stars']
+           },
+           {
+            model: SpotImage,
+            where: { preview: true },
+            attributes: ['url']
+           }
+        ],  
+    });
+    const spots = spotsPromise.map(spot => {
+        spot = spot.toJSON();
+
+        let starAvg = null;
+        if(spot.Reviews.length) {
+            starAvg = spot.Reviews.reduce((acc, curr) => acc + (curr.stars)/spot.Reviews.length, 0)
+        };
+        spot.avgRating = starAvg;
+
+        let url = null
+        if(spot.SpotImages.length){
+            url = spot.SpotImages[0].url
+        }
+
+        spot.previewImage = url
+        delete spot.Reviews;
+        delete spot.SpotImages;
+        return spot
+    })
+    res.json({Spots: spots})
 })
 
 module.exports = router;
