d3ae5d8f78a138590bade5225fdd34612f752c83
KatiePee <piele.katie@gmail.com>
2023-04-21 10:48:14 -0700

    errors and validations organized

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,8 +4,9 @@ const { Spot, Review, SpotImage, User } = require('../../db/models');
 const Sequelize =require('../../db/models')
 const cookieParser = require('cookie-parser');
 const {requireAuth} = require('../../utils/auth.js');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { handleValidationErrors, validateCreateSpot, validateEditSpot} = require('../../utils/validation');
+const { Op } = require('sequelize');
+const { spotNotFound, userNotFound, unauthorized } = require('../../utils/errors')
 
 router.get('/', async(_req, res) => {
     const spotsPromise = await Spot.findAll({
@@ -47,11 +48,7 @@ router.get('/', async(_req, res) => {
 
 router.get('/current', requireAuth,  async (req, res, next) => {
     if(!req.user){
-        const err = new Error('There is currently no user logged in');
-        err.title = 'There is currently no user logged in';
-        err.errors = { message: 'There is currently no user logged in' };
-        err.status = 401;
-        return next(err);
+    return userNotFound(next)
     }
     const currentUser = req.user.toJSON();
     const currentUserSpotsPromise = await Spot.findAll({
@@ -90,7 +87,15 @@ router.get('/current', requireAuth,  async (req, res, next) => {
         return spot
     })
     res.json({Spots: currentUserSpots})
-    
+})
+
+router.post('/', validateCreateSpot, requireAuth, async (req, res, next) => {
+    const user = req.user.toJSON();
+    const ownerId = user.id;
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const newSpot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price});
+
+    return res.status(201).json(newSpot)
 })
 
 router.get('/:spotId', async (req, res, next) => {
@@ -112,11 +117,7 @@ router.get('/:spotId', async (req, res, next) => {
         ]
     });
     if(!spot){
-        const err = new Error("Spot couldn't be found");
-        err.title = "Spot couldn't be found";
-        err.errors = { message: "Spot couldn't be found"};
-        err.status = 404;
-        return next(err);
+        return spotNotFound(next)
     }
     
     spot = spot.toJSON();
@@ -127,49 +128,46 @@ router.get('/:spotId', async (req, res, next) => {
     spot.numReviews = spot.Reviews.length;
     spot.avgStarRating = starAvg;
     delete spot.Reviews;    
-    res.json(spot)
-
+    return res.json(spot)
 })
 
-const validateSpot = [
-    check('address')
-        .not().isEmpty().withMessage('address is required')
-        .isLength({min: 1, max: 30}).withMessage('address must be between 1 and 30 character'),
-    check('city')
-        .not().isEmpty().withMessage('city is required')
-        .isLength({min: 1, max: 30}).withMessage('city must be between 1 and 30 characters'),
-    check('state')
-        .not().isEmpty().withMessage('state is required')
-        .isLength({min: 1, max: 30}).withMessage('state must be between 1 and 30 characters'),
-    check('country')
-        .not().isEmpty().withMessage('country is required')
-        .isLength({min: 1, max: 30}).withMessage('country must be between 1 and 30 characters'),
-    check('lat')
-        .not().isEmpty().withMessage('lat is required')
-        .isFloat({min: -90, max: 90}).withMessage('Invalid latitude. lat must be between -90 and 90'),
-    check('lng')
-        .not().isEmpty().withMessage('lng is required')
-        .isFloat({min: -180, max: 180}).withMessage('Invalid longitude. lng must be between -180 and 180'),
-    check('name')
-        .not().isEmpty().withMessage('name is required')
-        .isLength({min: 1, max: 30}).withMessage('name must be between 1 and 30 characters'),
-    check('description')
-        .not().isEmpty().withMessage('description is required')
-        .isLength({min: 1, max: 2048}).withMessage('description must be between 1 and 2048 characters'),
-    check('price')
-        .not().isEmpty().withMessage('price is required')
-        .isFloat({min: 0}).withMessage('price must be more than 0'),
-    handleValidationErrors
-]
-
-router.post('/', validateSpot, requireAuth, async (req, res, next) => {
+router.put('/:spotId', validateEditSpot, requireAuth, async (req, res, next) => {
     const user = req.user.toJSON();
-    const ownerId = user.id;
-    const { address, city, state, country, lat, lng, name, description, price } = req.body;
-    const newSpot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price});
+    const spotId = req.params.spotId;
+    const spot = await Spot.findByPk(spotId)
 
-    return res.json(newSpot)
+    if(!spot) return spotNotFound(next);
+    if(spot.ownerId !== user.id) return unauthorizedUser(next);
 
+    const {address, city, state, country, lat, lng, name, description, price} = req.body;
+    const keys = Object.keys(req.body);
 
+    keys.forEach(key => {
+        spot[key] = req.body[key]
+        console.log(req.body[key])
+    })
+    res.json('test')
 })
+
+router.post('/:spotId/images', requireAuth, async (req, res, next) => {
+    const user = req.user.toJSON();
+    const spotId = req.params.spotId;
+    const { url, preview } = req.body;
+    const spot = await Spot.findByPk(spotId);
+
+    if(!spot) return spotNotFound(next);
+    if(spot.ownerId !== user.id) return unauthorized(next);
+    
+    
+    const spotImage = await SpotImage.create({spotId, url, preview})  
+    console.log(spotId)
+    res.json({
+        "id": spotImage.id,
+        "url": spotImage.url,
+        "preview": spotImage.preview
+      })
+})
+
+
+
 module.exports = router;
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,18 +1,16 @@
 // backend/utils/validation.js
 const { validationResult } = require('express-validator');
+const { check } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
-  console.log(validationErrors)
   if (!validationErrors.isEmpty()) { 
     const errors = {};
     validationErrors
       .array()
-      .forEach(error => {
-        console.log(error)
-        errors[error.path] = error.msg});
+      .forEach(error => errors[error.path] = error.msg);
     
     const err = Error("Bad request.");
     err.errors = errors;
@@ -23,6 +21,70 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
-module.exports = {
+const validateEditSpot = [
+  check('address')
+      .optional()
+      .isLength({min: 1, max: 30}).withMessage('address must be between 1 and 30 character'),
+  check('city')
+      .optional()
+      .isLength({min: 1, max: 30}).withMessage('city must be between 1 and 30 characters'),
+  check('state')
+      .optional()
+      .isLength({min: 1, max: 30}).withMessage('state must be between 1 and 30 characters'),
+  check('country')
+      .optional()
+      .isLength({min: 1, max: 30}).withMessage('country must be between 1 and 30 characters'),
+  check('lat')
+      .optional()
+      .isFloat({min: -90, max: 90}).withMessage('Invalid latitude. lat must be between -90 and 90'),
+  check('lng')
+      .optional()
+      .isFloat({min: -180, max: 180}).withMessage('Invalid longitude. lng must be between -180 and 180'),
+  check('name')
+      .optional()
+      .isLength({min: 1, max: 30}).withMessage('name must be between 1 and 30 characters'),
+  check('description')
+      .optional()
+      .isLength({min: 1, max: 2048}).withMessage('description must be between 1 and 2048 characters'),
+  check('price')
+      .optional()
+      .isFloat({min: 0}).withMessage('price must be more than 0'),
   handleValidationErrors
+]
+
+const validateCreateSpot = [
+  check('address')
+      .not().isEmpty()
+      .isLength({min: 1, max: 30}).withMessage('address is required and must be between 1 and 30 character'),
+  check('city')
+      .not().isEmpty()
+      .isLength({min: 1, max: 30}).withMessage('city is required and must be between 1 and 30 characters'),
+  check('state')
+      .not().isEmpty()
+      .isLength({min: 1, max: 30}).withMessage('state is required and  must be between 1 and 30 characters'),
+  check('country')
+      .not().isEmpty()
+      .isLength({min: 1, max: 30}).withMessage('country is required and must be between 1 and 30 characters'),
+  check('lat')
+      .not().isEmpty()
+      .isFloat({min: -90, max: 90}).withMessage('Invalid latitude. lat must be between -90 and 90'),
+  check('lng')
+      .not().isEmpty()
+      .isFloat({min: -180, max: 180}).withMessage('Invalid longitude. lng must be between -180 and 180'),
+  check('name')
+      .not().isEmpty()
+      .isLength({min: 1, max: 30}).withMessage('name is required and must be between 1 and 30 characters'),
+  check('description')
+      .not().isEmpty()
+      .isLength({min: 1, max: 2048}).withMessage('description is required and must be between 1 and 2048 characters'),
+  check('price')
+      .not().isEmpty()
+      .isFloat({min: 0}).withMessage('price is required and  must be more than 0'),
+  handleValidationErrors
+]
+
+module.exports = {
+  handleValidationErrors,
+  validateEditSpot,
+  validateCreateSpot
 };
